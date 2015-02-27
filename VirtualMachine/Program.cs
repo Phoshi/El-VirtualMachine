523275f5fbfd1842a162b0191c684e9cf14571a3
Andrew Heaford <AHPhoshi@gmail.com>
2015-02-27 11:52:59 +0000

    VM now has verbosity switch
    Mostly to aid automated testing, but it's also a good idea for like, correctness purposes

--- VirtualMachine/Program.cs
+++ VirtualMachine/Program.cs
@@ -6,35 +6,41 @@ using Speedycloud.Bytecode;
 namespace Speedycloud.Runtime {
     class Program {
         static void Main(string[] args) {
-            var bytecodeData = File.ReadAllText(args[0]);
-            Console.WriteLine("Data:");
-            Console.WriteLine(bytecodeData);
+            var switches = args.Where(arg => arg[0] == '-').ToList();
+            var filename = args.First(arg => arg[0] != '-');
+            var bytecodeData = File.ReadAllText(filename);
             var deserialiser = new BytecodeSerialiser();
             var data = deserialiser.Load(bytecodeData);
 
             var i = 0;
             var consts = data.Item2.ToDictionary(elem => i++);
 
-            Console.WriteLine("Constants: ");
-            foreach (var pair in consts) {
-                Console.WriteLine("\t{0} = {1}", pair.Key, pair.Value);
-            }
+            if (switches.Contains("-v")) {
+                Console.WriteLine("Constants: ");
+                foreach (var pair in consts) {
+                    Console.WriteLine("\t{0} = {1}", pair.Key, pair.Value);
+                }
 
-            Console.WriteLine("Bytecode:");
-            var bytecodeCount = 0;
-            foreach (var opcode in data.Item1) {
-                Console.WriteLine("\t" + bytecodeCount++ + " " + opcode);
+                Console.WriteLine("Bytecode:");
+                var bytecodeCount = 0;
+                foreach (var opcode in data.Item1) {
+                    Console.WriteLine("\t" + bytecodeCount++ + " " + opcode);
+                }
+                Console.WriteLine("Program output:");
             }
 
             var vm = new VirtualMachine(data.Item1.ToList(), consts);
             vm.Run();
 
-            Console.WriteLine("Heap:");
-            foreach (var heap in vm.Heap) {
-                Console.WriteLine(heap);
-            }
+            if (switches.Contains("-v")) {
+                Console.WriteLine();
+                Console.WriteLine("Heap:");
+                foreach (var heap in vm.Heap) {
+                    Console.WriteLine(heap);
+                }
 
-            Console.ReadKey();
+                Console.ReadKey();
+            }
         }
     }
 }
